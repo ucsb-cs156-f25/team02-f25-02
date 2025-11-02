4d87193f53a3e468ca5ccf1062ae84c4efe3e7d5
Manny Lemus <ealemus@ucsb.edu>
2025-11-02 14:46:48 -0800

    ml - create fixtures for HelpRequest table

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,42 @@
+const helpRequestFixtures = {
+  oneHelpRequest: {
+    id: 1,
+    requesterEmail: "ealemus@ucsb.edu",
+    teamId: "f25-4pm-2",
+    tableOrBreakoutRoom: "2",
+    requestTime: "2025-10-01T16:30:00",
+    explanation: "add delete, put, get end points for helpRequest",
+    solved: false,
+  },
+  threeHelpRequests: [
+    {
+      id: 2,
+      requesterEmail: "aaaaaaa@ucsb.edu",
+      teamId: "f25-2pm-2",
+      tableOrBreakoutRoom: "2",
+      requestTime: "2025-10-02T16:30:00",
+      explanation: "Need help with Swagger-ui",
+      solved: false,
+    },
+    {
+      id: 3,
+      requesterEmail: "bbbbbbbb@ucsb.edu",
+      teamId: "f25-3pm-3",
+      tableOrBreakoutRoom: "3",
+      requestTime: "2025-10-03T16:30:00",
+      explanation: "Dokku problems",
+      solved: false,
+    },
+    {
+      id: 4,
+      requesterEmail: "cccccccc@ucsb.edu",
+      teamId: "f25-4pm-4",
+      tableOrBreakoutRoom: "4",
+      requestTime: "2025-10-04T16:30:00",
+      explanation: "Merge conflict",
+      solved: false,
+    },
+  ],
+=};
+
+export { helpRequestFixtures };
