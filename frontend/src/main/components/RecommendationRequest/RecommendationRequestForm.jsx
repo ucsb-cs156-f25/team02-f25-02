e899072467c3125dbdd6bb54e1d3f74412063372
Jeffrey Keem <keemaje5@gmail.com>
2025-11-04 03:49:23 -0800

    WIP on recommendation request formNeed to add Z for timezone on create and edit.Need to remove the Z that we get from the backend when filling in the initial contents.

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.jsx
@@ -19,6 +19,14 @@ function RecommendationRequestForm({
 
   const testIdPrefix = "RecommendationRequestForm";
 
+  // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+  // Note that even this complex regex may still need some tweaks
+
+  // Stryker disable Regex
+  const isodate_regex =
+    /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+  // Stryker restore Regex
+
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       {initialContents && (
@@ -71,7 +79,81 @@ function RecommendationRequestForm({
           })}
         />
         <Form.Control.Feedback type="invalid">
-          {errors.description?.message}
+          {errors.professorEmail?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="explanation">Explanation</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-explanation"}
+          id="explanation"
+          type="text"
+          isInvalid={Boolean(errors.explanation)}
+          {...register("explanation", {
+            required: "Explanation is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.explanation?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+            <Form.Label htmlFor="dateRequested">Date (in UTC)</Form.Label>
+            <Form.Control
+              data-testid={testIdPrefix + "-dateRequested"}
+              id="dateRequested"
+              type="datetime-local"
+              isInvalid={Boolean(errors.dateRequested)}
+              {...register("dateRequested", {
+                required: true,
+                pattern: isodate_regex,
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.dateRequested && "Date Requested is required. "}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="dateNeeded">Date Needed (in UTC)</Form.Label>
+            <Form.Control
+              data-testid={testIdPrefix + "-dateNeeded"}
+              id="dateNeeded"
+              type="datetime-local"
+              isInvalid={Boolean(errors.dateNeeded)}
+              {...register("dateNeeded", {
+                required: true,
+                pattern: isodate_regex,
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.dateNeeded && "Date Needed is required. "}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="done">Done</Form.Label>
+        <Form.Select
+          data-testid={testIdPrefix + "-done"}
+          id="done"
+          isInvalid={Boolean(errors.done)}
+          {...register("done", {
+            required: "Done is required.",
+          })}
+        >
+          <option value="">Select...</option>
+          <option value="false">false</option>
+          <option value="true">true</option>
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+          {errors.done?.message}
         </Form.Control.Feedback>
       </Form.Group>
 
