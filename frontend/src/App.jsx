0b92a5408aa7d280a3800d9f1b5c892cc858b7e6
Matt Blanke <mattheweblanke@gmail.com>
2025-11-04 00:34:13 -0800

    mb - add tests for UCSBOrganization Placeholder pages for Index, Edit, Create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,11 @@ function App() {
       )}
       {hasRole(currentUser, "ROLE_USER") && (
         <>
-          <Route exact path="/ucsborganizations" element={<UCSBOrganizationIndexPage />} />
+          <Route
+            exact
+            path="/ucsborganizations"
+            element={<UCSBOrganizationIndexPage />}
+          />
         </>
       )}
       {hasRole(currentUser, "ROLE_ADMIN") && (
--- frontend/src/stories/components/UCSBOrganizations/UCSBOrganizationTable.stories.jsx
+++ frontend/src/stories/components/UCSBOrganizations/UCSBOrganizationTable.stories.jsx
@@ -43,4 +43,3 @@ ThreeItemsAdminUser.parameters = {
     }),
   ],
 };
- 
