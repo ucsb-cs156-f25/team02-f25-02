e4c8f0f83fc445fe44894a55d5e45cf7fcbf6224
Manny Lemus <ealemus@ucsb.edu>
2025-11-05 14:14:31 -0800

    WIP: HelpRequestTable not in directory, need to perform a merge

--- frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.jsx
+++ frontend/src/main/pages/HelpRequests/HelpRequestsIndexPage.jsx
@@ -1,17 +1,46 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
 
-export default function HelpRequestsIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestsTable from "main/components/HelpRequests/HelpRequestsTable";
+import { useCurrentUser, hasRole } from "main/utils/useCurrentUser";
+import { Button } from "react-bootstrap";
+
+export default function HelpRequestIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const {
+    data: HelpRequests,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/helprequests/all"],
+    { method: "GET", url: "/api/helprequests/all" },
+    // Stryker disable next-line all : don't test default value of empty list
+    [],
+  );
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/helpRequests/create"
+          style={{ float: "right" }}
+        >
+          Create HelpRequest
+        </Button>
+      );
+    }
+  };
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/HelpRequests/create">Create</a>
-        </p>
-        <p>
-          <a href="/HelpRequests/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>HelpRequests</h1>
+        <HelpRequestsTable helpRequests={helpRequests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
